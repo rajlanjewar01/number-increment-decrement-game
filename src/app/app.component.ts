a418bde4afcdd28dcebc62e544801d8142f12e08
rajperficient01 <rajl.perficient@gmail.com>
2022-06-01 12:17:33 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestApp';
+  
+value:number = 0;
+
+getValue(event :any){
+  this.value = parseInt(event.target.value);
+}
+
+incr(){
+  this.value = this.value + 5;
+}
+decr(){
+  this.value = this.value - 1;
+}
+
 }
